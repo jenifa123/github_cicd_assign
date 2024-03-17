060908f578342b7d93806b5bc406d9f08e83e233
Jenifa <jenifanadar98@gmail.com>
2024-03-18 00:33:20 +0530

    Changed filename of S3

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,6 @@
 import boto3
 import pandas as pd
+from io import StringIO
 
 s3_client = boto3.client('s3')
 sns_client = boto3.client('sns')
@@ -18,8 +19,9 @@ def lambda_handler(event, context):
         print(s3_file_key)
         resp = s3_client.get_object(Bucket=bucket_name, Key=s3_file_key)
         print(resp['Body'])
-        data =  resp['Body'].read().decode('utf-8')
-        df_s3_data = pd.read_json(data)
+        data =  resp['Body'].read()
+        json_data = StringIO(data)
+        df_s3_data = pd.read_json(json_data)
         print('File read successfully')
         
         df_filtered_data = df_s3_data[df_s3_data['status']=='delivered']
@@ -27,14 +29,20 @@ def lambda_handler(event, context):
         #write data to json
         
         df_filtered_json_data =  df_filtered_data.to_json(output_file_name,orient='records')
-        
+        print(df_filtered_json_data)
         #upload json file to target bucket
         
-        with open('/tmp/2024-03-09-output.json', 'w') as f:
-            f.write(df_filtered_json_data)
+        # with open('/tmp/2024-03-09-output.json', 'w') as f:
+        #     f.write(df_filtered_json_data)
         
-        s3_client.upload_file('/tmp/2024-03-09-output.json', 'doordash-target-zonee', '2024-03-09-output.json')
+        # s3_client.upload_file('/tmp/2024-03-09-output.json', 'doordash-target-zonee', '2024-03-09-output.json')
         
+        try:
+            s3_client.put_object(Bucket='doordash-target-zonee', Key='output.json', Body=df_filtered_json_data)
+            print("Filtered DataFrame uploaded to S3 successfully.")
+        except Exception as e:
+            print(f"Error uploading filtered DataFrame to S3: {e}")
+            
         message = "Input S3 File {} has been processed succesfuly with order status as Delivered !!".format("s3://"+bucket_name+"/"+s3_file_key)
         response = sns_client.publish(Subject="Daily Data Processing and Filtering successful",TargetArn=sns_arn, Message=message, MessageStructure='text')
     except Exception as err:
